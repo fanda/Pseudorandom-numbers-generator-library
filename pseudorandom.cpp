92e2c9f81b9d4bf97a8c643d346f97cb7b57bb83
fanda <fandisek@gmail.com>
2010-12-10 21:35:33 +0100

    normalní opraveno

--- pseudorandom.cpp
+++ pseudorandom.cpp
@@ -17,9 +17,9 @@ double RandomGenerator::Normal(double mu, double sigma)
   double u, v, x, y, q;
   do {
     u = Random_real1();
-    v = 1.7156 * (Random_real1() - 0.5);
+    v = 1.7156 * (Random_real3() - 0.5); // scale v to (-r,r), r=(2/e)^0.5
     x = u - 0.449871;
-    y = std::abs(v) - 0.386595;
+    y = std::abs(v) + 0.386595;
     q = (x*x) + y * (0.19600 * y - 0.25472 * x);
   } while (q > 0.27597 && (q > 0.27846 || v*v > -4 * std::log(u)*u*u));
   return mu + sigma * v / u;
