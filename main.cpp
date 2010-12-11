7f958cbd3ac559aed8b525631af630eeca5b9ad6
ota <ota.pavelek@gmail.com>
2010-12-11 11:58:59 +0100

    prubezne

--- main.cpp
+++ main.cpp
@@ -18,35 +18,34 @@ int main(int argc, char *argv[])
     std::cout<< rg.Random() <<std::endl;
   std::cout<<std::endl;*/
 
-  for (int i = 0; i < 1000000; i++)
-    std::cout<< rg.Random_real1() <<std::endl;
+/*  for (int i = 0; i < 1000000; i++)
+    std::cout<< rg.Random_real1() <<std::endl;*/
   //std::cout<<std::endl;*/
 
-  /*for (int i = 0; i < 1000000; i++)
-    std::cout<< rg.Weibull(3,200) <<std::endl;
+/*  for (int i = 0; i < 10000; i++)
+    std::cout<< rg.Weibull(1.0, 5) <<std::endl;
   //std::cout<<std::endl;*/
 
-  /*for (int i = 0; i < 1000000; i++) {
-    double n = rg.Normal(1, 5);
-    if (n < 25. && n > -25.)
-      std::cout<< n <<std::endl;
+/*  for (int i = 0; i < 1000000; i++) {
+    std::cout<< rg.Uniform(1, 20)  <<std::endl;
   }*/
   //std::cout<<std::endl;*/
 
-  /*for (int i = 0; i < 10; i++)
-    std::cout<< rg.Poisson(5) <<std::endl;
+/*  for (int i = 0; i < 1000000; i++) {
+    std::cout<< rg.Exponential(0.5) <<std::endl;
+  }*/
 
-  for (int i = 0; i < 10; i++)
-    std::cout<< rg.Poisson(15) <<std::endl;*/
+/*  for (int i = 0; i < 1000000; i++)
+    std::cout<< rg.Normal(0, 5.0) <<std::endl;*/
 
-  /*for (int i = 0; i < 10; i++)
-    std::cout<< rg.Poisson(25) <<std::endl;*/
+/*  for (int i = 0; i < 1000000; i++)
+    std::cout<< rg.Poisson(10) <<std::endl;*/
 
   //std::cout<<std::endl;*/
    
-  /*for (int i = 0; i < 1000; i++) {
-    std::cout << rg.Normal(0,1) << std::endl;
-  }*/
+  for (int i = 0; i < 100000; i++) {
+    std::cout << rg.Gamma(5, 1) << std::endl;
+  }
   return 0;
 }
 
