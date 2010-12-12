f6add961265e7c05d251b6513aa7affe14ffeaf9
ota <ota.pavelek@gmail.com>
2010-12-12 03:39:32 +0100

    cast generovani obrazku

--- main.cpp
+++ main.cpp
@@ -9,11 +9,45 @@
 #include "pseudorandom.h"
 
 #include <ctime>
+#include <fstream>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
   RandomGenerator<MT32> rg((unsigned long)time(NULL));
   
+  std::ofstream file1, file2, file3;
+  //FIXME testovat jestli se povedlo
+  file1.open("exponential_3");
+  file2.open("exponential_1.5");
+  file3.open("exponential_1");
+
+  for (int i = 0; i < 100000; i++) {
+    file1 << rg.Exponential(3) << std::endl;
+    file2 << rg.Exponential(1.5) << std::endl;
+    file3 << rg.Exponential(1) << std::endl;
+  }
+
+  file1.close();
+  file2.close();
+  file3.close();
+
+
+  //FIXME testovat jestli se povedlo
+  file1.open("normal_0_1");
+  file2.open("normal_-2_0.5");
+  file3.open("normal_0_5");
+
+  for (int i = 0; i < 100000; i++) {
+    file1 << rg.Normal(0, 1) << std::endl;
+    file2 << rg.Normal(-2, std::sqrt(0.5)) << std::endl;
+    file3 << rg.Normal(0, std::sqrt(5)) << std::endl;
+  }
+
+  file1.close();
+  file2.close();
+  file3.close();
+
 /*  for (int i = 0; i < 10; i++)
     std::cout<< rg.Uniform(1,100) <<std::endl;
   std::cout<<std::endl;*/
@@ -22,23 +56,19 @@ int main(int argc, char *argv[])
     std::cout<< rg.Random_real1() <<std::endl;*/
   //std::cout<<std::endl;*/
 
-  for (int i = 0; i < 1000000; i++)
-    std::cout<< rg.Weibull(1, 5) <<std::endl;
+/*  for (int i = 0; i < 100000; i++)
+    std::cout<< rg.Weibull(1.5, 1) <<std::endl;*/
   //std::cout<<std::endl;*/
 
-/*  for (int i = 0; i < 1000000; i++) {
-    std::cout<< rg.Uniform(1, 20)  <<std::endl;
+/*  for (int i = 0; i < 100000; i++) {
+    std::cout<< rg.Gamma(2, 2)  <<std::endl;
   }*/
   //std::cout<<std::endl;*/
 
-/*  for (int i = 0; i < 1000000; i++) {
-    std::cout<< rg.Exponential(0.5) <<std::endl;
-  }*/
+/*  for (int i = 0; i < 100000; i++)
+    std::cout<< rg.Normal(-2, std::sqrt(0.5)) <<std::endl;*/
 
-/*  for (int i = 0; i < 1000000; i++)
-    std::cout<< rg.Normal(0, 5.0) <<std::endl;*/
-
-/*  for (int i = 0; i < 1000000; i++)
+/*  for (int i = 0; i < 100000; i++)
     std::cout<< rg.Poisson(10) <<std::endl;*/
 
   //std::cout<<std::endl;*/
@@ -48,6 +78,8 @@ int main(int argc, char *argv[])
     if (i < 50)
     std::cout << i << std::endl;
   }*/
+
+  system("gnuplot skript");
   return 0;
 }
 
--- pseudorandom.h
+++ pseudorandom.h
@@ -48,7 +48,7 @@ RandomGenerator<UniformGenerator>::Normal(double mu, double sigma)
     x = u - 0.449871;
     y = std::abs(v) + 0.386595;
     q = (x*x) + y * (0.19600 * y - 0.25472 * x);
-  } while (q > 0.27597 && (q > 0.27846 || v*v > -4 * std::log(u)*u*u));
+  } while (q > 0.27597 && (q > 0.27846 || v*v > -4. * std::log(u)*u*u));
   return mu + sigma * v / u;
 }
 
@@ -65,7 +65,7 @@ RandomGenerator<UniformGenerator>::Weibull(double shape, double scale)
     /*if (shape < 1) TODO
       return +Inf;*/
   }
-  return scale * std::pow(-1.* std::log(1.- random_number), 1./ shape);
+  return scale * std::pow(-1.* std::log(random_number), 1./ shape);
 }
 
 template <class UniformGenerator>
